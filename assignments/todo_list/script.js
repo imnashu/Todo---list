4df11ae38a1aa9ab6cae687b696fee29aabaa210
imnashu07 <imnashu07@gmail.com>
2021-12-06 20:01:02 +0530

    Final

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,18 +1,18 @@
-function createtodo(){
-    var tag = document.createElement("p");
-    tag.setAttribute("key",index)
-    index+=1
-    tag.addEventListener("click",function(){
+function createtodo()
+{
+    var tags = docs.createElement("p");
+    tags.setAttribute("key",i)
+    i += 1
+    tags.addEventListener("click",function()
+    {
         this.remove()
     })
-    var newtodo = document.getElementsByTagName('input')[0].value
-    var text = document.createTextNode(newtodo)
-
-    var element = document.getElementsByClassName("todos")[0]
-    tag.appendChild(text)
-    element.appendChild(tag)
-
+    var newtodo = docs.getElementsByTagName('input')[0].value
+    var text = docs.createTextNode(newtodo)
+    var element = docs.getElementsByClassName("todos")[0]
+    tags.appendChild(text)
+    element.appendChild(tags)
 }
-var index=0
-var form=document.getElementsByTagName("button")[0]
-form.addEventListener('click',createtodo)
+var i = 0
+var list = docs.getElementsByTagName("button")[0]
+list.addEventListener('click',createtodo)
